462d9ac31d3abd8d8b3ce9f4afe8997cbb5acea4
AlexanderChristian <acignat@gmail.com>
2024-10-08 09:48:29 +0300

    Refactor, remove unecessary files

--- src/main/java/com/example/sensorapp/Domain/Producers/SensorProducer.java
+++
@@ -1,8 +0,0 @@
-package com.example.sensorapp.Domain.Producers;
-
-import com.example.sensorapp.Domain.Common.SensorMessage;
-//not needed anymore on this part
-public interface SensorProducer {
-    SensorMessage generateData();
-    String getId();
-}
--- src/main/java/com/example/sensorapp/Scratchpad.java
+++
@@ -1,8 +0,0 @@
-package com.example.sensorapp;
-
-public class Scratchpad {
-
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
